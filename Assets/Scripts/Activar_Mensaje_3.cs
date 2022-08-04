af9df3bb197093264675b25d79508f294e7e6cc9
D0JYAN <jesussjg811@gmail.com>
2022-08-03 19:56:10 -0500

    Colocación de mensajes e interacción con npc
    Se agregaron los diferentes mensajes emergentes necesarios para que el usuario entienda las mecánicas del juego y se agregó la interacción con le npc

---
+++ Assets/Scripts/Activar_Mensaje_3.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activar_Mensaje_3 : MonoBehaviour
+{
+    //Esta linea llama el objeto del con el cual queremos que interactue el trigger
+    [SerializeField] private GameObject Mensaje_3;
+    private SpriteRenderer spr;
+
+    private void Start()
+    {
+        //Con este fragmento de codigo, llamamos la pripiedad de SpriteRender de nuestro objeto, para obtener su color y trasparencia.
+        spr = Mensaje_3.GetComponent<SpriteRenderer>();
+        Color Color_Mensaje_3 = spr.material.color;
+        Color_Mensaje_3.a = 0.0f;
+        spr.material.color = Color_Mensaje_3;
+    }
+
+    //Este condicional sirven para detectar cuando el Player colisiona con el trigger que creamos
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine("FadeIn");
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //La corrutinas son funciones que se van a ejecutar a la par de los FixedUpdate o los Update
+            //y son independientes de toda la programacion del mismo juego.
+            StartCoroutine("FadeOut");
+        }
+    }
+
+    IEnumerator FadeIn()
+    {
+        for (float i = 0.0f; i <= 1; i += 0.02f)
+        {
+            Color Color_Mensaje_3 = spr.material.color;
+            Color_Mensaje_3.a = i;
+            spr.material.color = Color_Mensaje_3;
+            yield return (0.05f);
+        }
+    }
+
+    IEnumerator FadeOut()
+    {
+        for (float i = 1; i >= 0; i -= 0.02f)
+        {
+            Color Color_Mensaje_3 = spr.material.color;
+            Color_Mensaje_3.a = i;
+            spr.material.color = Color_Mensaje_3;
+            yield return (0.05f);
+        }
+    }
+}
