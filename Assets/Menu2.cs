7f36ba95b86577d11281caa3b403b6ba3e7cd5f6
angelTBD <afuo02.afuo@gmail.com>
2025-04-01 15:25:00 -0600

    correcion 2 menu 2
    se corrigio el codigo del persistent ya que marcaba error

--- Assets/Menu2.cs
+++ Assets/Menu2.cs
@@ -12,7 +12,7 @@ public class Menu2 : MonoBehaviour
 
     private void Start()
     {
-        archivoGuardado = Application.persistentdataPath + "/datosJuego.json";
+        archivoGuardado = Application.persistentDataPath + "/datosJuego";
         CargarUltimaEscena();
     }
 
