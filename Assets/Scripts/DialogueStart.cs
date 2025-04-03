f22608a210add20f6c590b0345f5dec1f857daad
Da Internet <diego-canto@hotmail.com>
2025-04-03 14:23:52 -0600

    Progreso de Dialogo
    El NPC ahora puede detectar cuando estas en el rango de conversación y aparecer un signo para iniciar la conversación

--- Assets/Scripts/DialogueStart.cs
+++ Assets/Scripts/DialogueStart.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public class DialogueStart : MonoBehaviour
 {
+
+    [SerializeField] private GameObject dialogueMark;
+
     private bool isPlayerInRange;
 
     // Update is called once per frame
@@ -13,8 +16,24 @@ public class DialogueStart : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        isPlayerInRange = true;
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            isPlayerInRange = true;
+            dialogueMark.SetActive(true);
+            Debug.Log("Se puede iniciar dialogo");
+        }
+        
     }
 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
+            dialogueMark.SetActive(false);
+            Debug.Log("No se puede iniciar dialogo");
+        }
+            
+    }
 
 }
