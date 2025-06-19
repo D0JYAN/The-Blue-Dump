dcf63578693b68002a9318788d57deef9afbccff
angelTBD <afuo02.afuo@gmail.com>
2025-06-19 15:47:13 -0200

    intento de salto de nivel la pantalla de demo

---
+++ Assets/Scripts/Salto_Demo.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Salto_Demo : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            ControladorDatosJuego controlador = FindObjectOfType<ControladorDatosJuego>();
+            if (controlador != null)
+            {
+                controlador.GuardarDatos();
+                Cargar_Demo.nivel_Carga_demo("MainMenu");
+                Debug.Log("Punto de partida guardado");
+            }
+            else
+            {
+                Debug.LogError("No se encontró ControladorDatosJuego en la escena.");
+            }
+        }
+    }
+}
